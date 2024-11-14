ef57b6c34349435208d47507adeeeb769b6008fb
AsuraMTG <eper.karma@gmail.com>
2024-11-14 14:45:41 +0100

    2024.11.14 DELETE

--- 2024-11-14/app.js
+++ 2024-11-14/app.js
@@ -14,8 +14,7 @@ const connection = connect();
 
 app.use(express.json());
 
-if (connection) 
-{
+if (connection) {
     console.log('Connected to the MySQL server.');
 }
 
@@ -24,34 +23,33 @@ app.use(express.json());
 app.get('/rendeles/:id', (req, res) => {
     let id = req.params.id;
     let sql = "SELECT pizza.pnev, pizza.par, tetel.db FROM pizza NATURAL JOIN tetel WHERE tetel.razon = ?";
-    connection.query(sql, [id], function (err, results) { 
+    connection.query(sql, [id], function (err, results) {
         if (err) {
-            console.error(err.message, err.code); 
+            console.error(err.message, err.code);
             res.status(500).send('Database error');
         } else {
             res.status(201).send(results);
         }
     });
-
 });
 
 app.post('/rendeles', (req, res) => {
     const data = req.body;
     let sql = "INSERT INTO `rendeles` (`razon`, `vazon`, `fazon`, `idopont`) VALUES (NULL, ?, ?, current_timestamp())";
-    connection.query(sql, [data.vazon, data.fazon], function (err, results) { 
+    connection.query(sql, [data.vazon, data.fazon], function (err, results) {
         if (err) {
-            console.error(err.message, err.code); 
+            console.error(err.message, err.code);
             res.status(500).send('Database error');
         } else {
             const rendelesId = results.insertId;
             for (let index = 0; index < data.items.length; index++) {
                 const element = data.items[index];
                 sql = "INSERT INTO `tetel` (`razon`, `pazon`, `db`) VALUES (?, ?, ?)";
-                connection.query(sql, [rendelesId, element.pazon, element.db], function (err, results) { 
+                connection.query(sql, [rendelesId, element.pazon, element.db], function (err, results) {
                     if (err) {
-                        console.error(err.message, err.code); 
+                        console.error(err.message, err.code);
                         res.status(500).send('Database error');
-                    } 
+                    }
                     res.status(201).send(results);
                 });
             }
@@ -60,7 +58,18 @@ app.post('/rendeles', (req, res) => {
     });
 });
 
-
+app.delete('/rendeles/:id', function (req, res) {
+    const id = req.params.id;
+    let sql = "DELETE FROM rendeles WHERE rendeles.razon = ?";
+    connection.query(sql, [id], function (err, results) {
+        if (err) {
+            console.error(err.message, err.code);
+            res.status(500).send('Database error');
+        } else {
+            res.status(201).send(results);
+        }
+    });
+});
 
 app.listen(3000, () => {
     console.log('A szerver elindult a http://localhost:3000 porton.');
